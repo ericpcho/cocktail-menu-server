816454d54e968c8dd51d8e497c42f5e2b9a1a1b9
ericpcho <ericpcho@gmail.com>
2017-10-16 12:03:41 -0700

    added instanbul to gitignore and hooked up a local database in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 exports.DATABASE_URL =
 global.DATABASE_URL ||
 process.env.DATABASE_URL ||
-'mongodb://localhost/jwt-auth-demo';
+'mongodb://localhost/cocktails-menu';
 exports.PORT = process.env.PORT || 8080;
