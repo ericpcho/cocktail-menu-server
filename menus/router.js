61c62145b93d45835a61a99283023f7eaaafb48f
ericpcho <ericpcho@gmail.com>
2017-10-18 14:53:43 -0700

    changed name of result json

--- menus/router.js
+++ menus/router.js
@@ -41,8 +41,8 @@ router.post('/', jsonParser, (req, res) => {
 router.get('/:id', (req, res) => {
   Menus
     .findById(req.params.id)
-    .then(planet => {
-      res.status(200).json(planet);
+    .then(menu => {
+      res.status(200).json(menu);
     })
     .catch(() => {
       res.status(500).json({error: 'Something went wrong'}); 
